81d1f128472c5287b937a9d15e081e7d8ef099a0
Tonyy107 <antonmagdy53@gmail.com>
2023-07-25 18:14:46 -0700

    lol

--- main_shell.c
+++ main_shell.c
@@ -1,1 +1,33 @@
+#include <main.h>
 
+/**
+ * main - entry point
+ * Return: 0 or 1
+ */
+int main(void)
+{
+	char *rem;
+	int lol;
+	char command;
+	int stat;
+
+	while (1)
+	{
+		printf("$dola$ ");
+		lol = getline(&rem, sizeof(rem), stdin);
+		if (lol == -1)
+		{
+			perror("end of file\n")
+				break;
+		}
+		else 
+		{
+			command = line;
+			stat = system(command);
+			if (stat != 0)
+				printf("'%s' not found",command);
+		}
+
+	}
+	return (0);
+}
