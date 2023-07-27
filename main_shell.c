2f89c7cccb0a6aac42200a1686c628b016a219e4
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-26 17:13:27 -0700

    z

--- main_shell.c
+++ main_shell.c
@@ -7,44 +7,44 @@
  */
 int main(void)
 {
-    char *argv[] = {"/bin/ls", "-l", NULL};
-    int output;
-    char *buffer;
-    size_t bufsize = 32;
-    char *command;
-    pid_t pid;
+	char *argv[] = {"/bin/ls", "-l", NULL};
+	int output;
+	char *buffer;
+	size_t bufsize = 32;
+	char *command;
+	pid_t pid;
 
-    pid = fork();
+	pid = fork();
 
-    while (1)
-    {
-        buffer = (char *)malloc(bufsize * sizeof(char));
-        getline(&buffer, &bufsize, stdin);
+	while (1)
+	{
+		buffer = (char *)malloc(bufsize * sizeof(char));
+		getline(&buffer, &bufsize, stdin);
 
-        if (buffer == argv[0])
-        {
-            perror("./hsh");
-        }
-        else
-        {
-            command = buffer;
-            output = system(command);
-            if (output != 0)
-                printf("command '%s' fatel .\n", command);
-            if (pid == 0)
-            {
-                printf("$ \n");
-                command = buffer;
-                output = system(command);
-                if (output != 0)
-                    printf("command '%s' fatel .\n", command);
-                wait(&output);
-            }
-        }
-        if (buffer != NULL)
-        {
-            free(buffer);
-        }
-    }
-    return (0);
+		if (buffer == argv[0])
+		{
+			perror("./hsh");
+		}
+		else
+		{
+			command = buffer;
+			output = system(command);
+			if (output != 0)
+				printf("command '%s' fatel .\n", command);
+			if (pid == 0)
+			{
+				printf("$ \n");
+				command = buffer;
+				output = system(command);
+				if (output != 0)
+					printf("command '%s' fatel .\n", command);
+				wait(&output);
+			}
+		}
+		if (buffer != NULL)
+		{
+			free(buffer);
+		}
+	}
+	return (0);
 }
